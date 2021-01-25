4e0921fdf69ccfb5a4d9ff1563b7151355f55650
Gorakhnath Ghadage <innovating.solution@gmail.com>
2021-01-25 10:34:36 +0530

    Creation on Custom Observable for http stream

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, noop, Observable } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,25 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+ /** The below is the example of the creation of Observable using promise and following the Observable Contract */
+    const http$ = new Observable(observer => {
+      fetch('/api/courses')
+      .then( response => {
+        return response.json();
+      }).then( body => {
+        observer.next(body);
+        observer.complete();
+
+      }).catch( err => {
+        observer.error(err);
+      })
+    });
+
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    );
   }
 
 }
