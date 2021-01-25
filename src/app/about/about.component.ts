3993903841ddd2357bc5a8cd34989b07b89b4298
Gorakhnath Ghadage <innovating.solution@gmail.com>
2021-01-26 05:09:37 +0530

    use of Concat Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,6 @@
-import { map } from 'rxjs/operators';
+import {  map } from 'rxjs/operators';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { interval, noop, Observable } from 'rxjs';
+import { concat, interval, noop, Observable, of } from 'rxjs';
 import { createHttpObservable } from '../common/util';
 
 @Component({
@@ -13,7 +13,21 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
- 
+
+    /** Below is the example of concat observable, which sequently concates the streams of data which it supposed to contact,
+     * this means that once the source1 complete the emition of data, the further sources of data are emitted and concatenated.
+     */
+    const source1$ = of(1, 2, 3);
+
+    const source2$ = of(4, 5, 6);
+
+    const source3$ = of(4, 5, 6);
+
+
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.subscribe(console.log);
+
   }
 
 
